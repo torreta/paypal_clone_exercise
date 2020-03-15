5b2a7d452c2761bf51993736428610d91b969318
Luis Campos <luiscampostorreta@gmail.com>
2020-03-15 18:42:03 -0400

    creating seed

--- paypal-backend/db/seeds.rb
+++ paypal-backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user1 = User.new
+user1.email = 'test@gmail.com'
+user1.password = '123123123'
+user1.password_confirmation = '123123123'
+user1.save
+
