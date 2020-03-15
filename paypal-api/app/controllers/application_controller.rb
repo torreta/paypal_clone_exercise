c3c7ef3f3c6bd8eb4c3fbc5c991e9c64f5e5fd8b
Luis Campos <luiscampostorreta@gmail.com>
2020-03-14 22:09:35 -0400

    aplication controller against forgetry

--- paypal-api/app/controllers/application_controller.rb
+++ paypal-api/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+        # este cambio solo es valido si todo ltus request son por el apo
+        protect_from_forgery unless: -> { request.format.json? }
         include DeviseTokenAuth::Concerns::SetUserByToken
 end
