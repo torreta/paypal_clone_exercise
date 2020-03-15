4c452acb543ab9f61e76b07c8b8c15a7a25f752e
Luis Campos <luiscampostorreta@gmail.com>
2020-03-14 21:31:16 -0400

    no forgeries denials

--- paypal-api/config/initializers/application_controller_renderer.rb
+++ paypal-api/config/initializers/application_controller_renderer.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-# ActiveSupport::Reloader.to_prepare do
+class ApplicationController < ActionController::Base
+    protect_from_forgery unless: -> { request.format.json? }
+  end
+
+  # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
 #     https: false
