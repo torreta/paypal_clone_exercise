fa525049f90da704c8269f469d72c0be2b1a1ebd
Luis Campos <luiscampostorreta@gmail.com>
2020-03-15 20:55:39 -0400

    flash bug squashed here too

--- paypal-backend/config/application.rb
+++ paypal-backend/config/application.rb
@@ -24,7 +24,7 @@ module PaypalBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.load_defaults 6.0 and config.autoloader = :classic
-
+    config.middleware.use ActionDispatch::Flash
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
