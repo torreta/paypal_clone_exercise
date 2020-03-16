83bdaaaa53d74b6faf952378b1780aae25c5ae7c
Luis Campos <luiscampostorreta@gmail.com>
2020-03-15 20:05:45 -0400

    crsf problem solved and seeds ran, and more logs

--- paypal-backend/app/controllers/application_controller.rb
+++ paypal-backend/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+    protect_from_forgery unless: -> { request.format.json? }
+    protect_from_forgery with: :null_session
     include Knock::Authenticable
 end
--- paypal-backend/app/controllers/user_token_controller.rb
+++ paypal-backend/app/controllers/user_token_controller.rb
@@ -1,2 +1,3 @@
 class UserTokenController < Knock::AuthTokenController
+    skip_before_action :verify_authenticity_token, :only => [:create, :index, :show]
 end
