1a529324749f433e392f17b58b25f186f99e2f2b
Luis Campos <luiscampostorreta@gmail.com>
2020-03-16 23:21:23 -0400

    more seeds of tests

--- paypal-backend/db/seeds.rb
+++ paypal-backend/db/seeds.rb
@@ -13,3 +13,27 @@ user1.password = '123123123'
 user1.password_confirmation = '123123123'
 user1.save
 
+user2 = User.new
+user2.email = 'test2@gmail.com'
+user2.password = '123123123'
+user2.password_confirmation = '123123123'
+user2.save
+
+user3 = User.new
+user3.email = 'test3@gmail.com'
+user3.password = '123123123'
+user3.password_confirmation = '123123123'
+user3.save
+
+user4 = User.new
+user4.email = 'test4@gmail.com'
+user4.password = '123123123'
+user4.password_confirmation = '123123123'
+user4.save
+
+user5 = User.new
+user5.email = 'test5@gmail.com'
+user5.password = '123123123'
+user5.password_confirmation = '123123123'
+user5.save
+
