b5a0e1cba00ec61d5d7697c5bc32d11652f0a54b
Luis Campos <luiscampostorreta@gmail.com>
2020-03-18 23:12:40 -0400

    deleting unused proyect

--- paypal-api/app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::Base
-        # este cambio solo es valido si todo ltus request son por el apo
-        protect_from_forgery unless: -> { request.format.json? }
-        include DeviseTokenAuth::Concerns::SetUserByToken
-end
